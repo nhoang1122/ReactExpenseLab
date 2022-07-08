cf0f8d62861ba370307e991451fa3d2664663660
Nhat Hoang <johnhoanghtx@gmail.com>
2022-07-08 11:31:34 -0500

    Initial

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css'
+
+const ExpenseItem = (props) => { 
+
+  //USE STATE RETURNS ARR WITH 2 ELEMENTS//
+
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
